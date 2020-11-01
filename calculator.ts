51b3edf31980cd06d52d785dc4bd73ac39cd880f
Alfred Asare <alfredasare101@gmail.com>
2020-11-01 19:17:25 +0000

    initial commit

---
+++ calculator.ts
@@ -0,0 +1,22 @@
+type Operation = 'multiply' | 'add' | 'divide';
+type Result = number;
+
+export const calculator = (a: number, b: number, op: Operation): Result => {
+    switch(op) {
+        case 'multiply':
+            return a * b;
+        case 'divide':
+            if( b === 0) throw new Error('Can\'t divide by 0!');
+            return a / b;
+        case 'add':
+            return a + b;
+        default:
+            throw new Error('Operation is not multiply, add or divide!');
+    }
+}
+
+try {
+    console.log(calculator(1,5, 'divide'));
+} catch (e) {
+    console.log('Something went wrong, error message', e.message);
+}
